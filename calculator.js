2530ceac2fe74037232ef3cf1989758a1143275e
fasihkhan754 <115379095+fasihkhan754@users.noreply.github.com>
2022-12-25 13:00:19 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,69 @@
+#! /usr/bin/node env
+import inquirer from "inquirer";
+function welcomeMsg(Msg) {
+    const answers = Msg;
+    console.log(answers);
+    getInputFormUser();
+    // const inquirer.prompt()
+}
+welcomeMsg("Hey there! Perform Your Calculation :)");
+// console.log('ahmad');
+async function getInputFormUser() {
+    const answer = await inquirer.prompt([
+        {
+            name: "firstNumber",
+            message: "Enter first number",
+            type: "input",
+            validate: function (input) {
+                if (isNaN(input)) {
+                    return "Not a valid Input";
+                }
+                else {
+                    return true;
+                }
+            }
+        },
+        {
+            name: "operation",
+            message: "Choose as operation",
+            type: "list",
+            choices: ["+", "-", "x", "/", "^", "%"]
+        },
+        {
+            name: "secondNumber",
+            message: "Enter Second Number",
+            type: "input",
+            validate: function (input) {
+                if (isNaN(input)) {
+                    return "Not a valid Input";
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+    ]);
+    console.log(answer);
+    const first = Number(answer.firstNumber);
+    const secnumber = Number(answer.secondNumber);
+    switch (answer.operation) {
+        case "+":
+            console.log(first + secnumber);
+            break;
+        case "-":
+            console.log(first - secnumber);
+            break;
+        case "x":
+            console.log(first * secnumber);
+            break;
+        case "/":
+            console.log(first / secnumber);
+            break;
+        case "^":
+            console.log(first ** secnumber);
+            break;
+        case "%":
+            console.log(first % secnumber);
+            break;
+    }
+}
